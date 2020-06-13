712afc11d400b27a86a29946767803c189063633
Castoco <casto2010@yandex.ru>
2020-06-13 20:14:32 +0300

    Оптимизировал под Мозилу

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 let showbuton = document.querySelector(".booking-button");
 let box = document.querySelector(".form-booking");
-let arrival = document.querySelector("[name=arrival")
-var form = document.querySelector('.booking-field')
+let arrival = document.querySelector("[name=arrival");
+var form = document.querySelector('.booking-field');
+var dayout = document.querySelector("[name=checkout]");
 
 showbuton.onclick = function () {
-  box.classList.toggle('black-hole')
-  box.classList.toggle('bounce')
+  box.classList.toggle('black-hole');
+  box.classList.toggle('bounce');
   arrival.focus();
-}
+  box.classList.remove('shake')
+};
+
 
 form.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  console.log('Получилось!');
-  console.log(arrival.value);
+  if (!arrival.value || !dayout.value) {
+    evt.preventDefault();
+    console.log(dayout.value)
+    box.classList.remove('shake')
+    box.offsetWidth = box.offsetWidth;
+    box.classList.add('shake')
+  }
 });
