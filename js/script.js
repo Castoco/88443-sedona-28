d7fbc9658b0e96cd36ceaceb203ed89d788b6486
Castoco <casto2010@yandex.ru>
2020-06-15 22:27:02 +0300

    Изменил анимацию

--- js/script.js
+++ js/script.js
@@ -4,20 +4,20 @@ let arrival = document.querySelector("[name=arrival");
 var form = document.querySelector('.booking-field');
 var dayout = document.querySelector("[name=checkout]");
 
-showbuton.onclick = function () {
+showbuton.addEventListener('click', function () {
   box.classList.toggle('black-hole');
-  box.classList.toggle('bounce');
   arrival.focus();
   box.classList.remove('shake')
-};
+});
 
 
 form.addEventListener("submit", function (evt) {
   if (!arrival.value || !dayout.value) {
     evt.preventDefault();
-    console.log(dayout.value)
     box.classList.remove('shake')
     box.offsetWidth = box.offsetWidth;
     box.classList.add('shake')
   }
 });
+
+
